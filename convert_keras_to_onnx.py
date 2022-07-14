733ef4b4c3a3d5ff4d7f959dee5f5ec7a5a6d2e0
Zeyu Yao <novodoodle@gmail.com>
2022-07-14 14:59:44 +0800

    Update Keras model name

--- convert_keras_to_onnx.py
+++ convert_keras_to_onnx.py
@@ -4,11 +4,11 @@ import onnx
 
 dir = './models/'
 
-model = tf.keras.models.load_model(dir + "model.h5")
-print("Loaded " + dir + "model.h5")
+model = tf.keras.models.load_model(dir + "keras_model.h5")
+print("Loaded " + dir + "keras_model.h5")
 
 input_signature = [tf.TensorSpec(list(model.input_shape), tf.float32, name='image_inputs')]
 onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature, opset=13)
-onnx.save(onnx_model, dir + "model.onnx")
+onnx.save(onnx_model, dir + "onnx_model.onnx")
 
-print("ONNX model saved to " + dir + "model.onnx")
+print("ONNX model saved to " + dir + "onnx_model.onnx")
