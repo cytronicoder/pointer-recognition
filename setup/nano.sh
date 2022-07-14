4165dcc7590b24df1e04617662f45183a78f6956
Zeyu Yao <novodoodle@gmail.com>
2022-07-14 15:08:26 +0800

    Check if jetson-inference repository exists

--- setup/nano.sh
+++ setup/nano.sh
@@ -11,9 +11,12 @@ apt-get install git cmake
 
 # Clone the jetson-inference project
 echo "Cloning the jetson-inference project..."
-git clone --recursive https://github.com/dusty-nv/jetson-inference
-cd jetson-inference
+if [ -d "jetson-inference" ]; then
+    echo "jetson-inference directory detected. Skipping cloning..."
+else
+    git clone --recursive https://github.com/dusty-nv/jetson-inference
 
 # Run docker/run.sh
 echo "Running docker/run.sh..."
+cd jetson-inference
 docker/run.sh
